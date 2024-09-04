d2b34d3be561b075f0ccba3ff152b68e4cf0666b
JASalcedoM <a.salcedo.morales@outlook.com>
2024-09-04 16:52:33 -0600

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-e39eae66-7521-4e40-b140-53b861f1c85b.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-5e27e8ff-d3c4-44a7-87d6-504f92b7ce5e.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
